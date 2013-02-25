78a75b8fdc53c54ed9c4c0be22a06109eb51ddda
Yak! <yak_ex@mx.scn.tv>
2013-02-26 07:42:42 +0900

    Merge script template into script (close gh-2).

--- template.pl
+++ template.pl
@@ -95,6 +95,26 @@ __[ pl ]__
 
 use strict;
 use warnings;
+
+use Getopt::Std;
+use Pod::Usage;
+
+my %opts;
+getopts('h', \%opts);
+pod2usage(-verbosity => 2) if exists $opts{h};
+pod2usage(-msg => '', -verbose => 0, -exitval => 1) if ...;
+
+\__END__
+
+\=head1 NAME
+
+{$file} - {$abstract}
+
+\=head1 SYNOPSIS
+
+\=head1 DESCRIPTION
+
+\=cut
 __[ pm ]__
 #
 #   {$file}: {$abstract}
