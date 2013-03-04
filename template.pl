6fce4b41596557839da28a734548ebc399ef4252
Yak! <yak_ex@mx.scn.tv>
2013-03-05 05:41:57 +0900

    Fix missing escape for curly brackets.

--- template.pl
+++ template.pl
@@ -172,7 +172,7 @@ use Pod::Usage;
 
 my %opts;
 getopts('h', \%opts);
-pod2usage(-verbosity => 2) if exists $opts{h};
+pod2usage(-verbosity => 2) if exists $opts\{h\};
 pod2usage(-msg => '', -verbose => 0, -exitval => 1) if ...;
 
 \__END__
