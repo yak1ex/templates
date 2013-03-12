7fb8e6a526decc61a3ffc141b18c90e71a0e12d0
Yak! <yak_ex@mx.scn.tv>
2013-03-12 17:16:19 +0900

    Add initial implement for ghclone.pl.

---
+++ ghclone.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+#
+#   ghclone.pl: Clone with user configuration
+#
+#   Written by Yasutaka ATARASHI <yak_ex@mx.scn.tv>
+#
+#   Distributed under the terms of The zlib License
+#   See http://www.zlib.net/zlib_license.html
+#
+#   $Id$
+#
+
+use strict;
+use warnings;
+
+use Getopt::Std;
+use Pod::Usage;
+
+use Config::Tiny;
+
+my %opts;
+getopts('h', \%opts);
+pod2usage(-verbosity => 2) if exists $opts{h};
+pod2usage(-msg => 'At least one repo MUST be specified', -verbose => 0, -exitval => 1) if ! @ARGV;
+
+my $conf_user = Config::Tiny->read("$ENV{HOME}/.gitconfig");
+my $def_user  = $conf_user->{user}{name};
+my $def_email = $conf_user->{user}{email};
+
+while(my $repo = shift) {
+	warn "$repo already exists" and next if -e $repo;
+	system "git clone https://github.com/yak1ex/${repo}.git";
+	my $conf_repo = Config::Tiny->read("${repo}/.git/config");
+	if(-f "${repo}/dist.ini" || -f "${repo}/Makefile.PL" || -f "${repo}/Build.PL") { # Perl module
+		$conf_repo->{user}{name}  = 'Yasutaka ATARASHI';
+		$conf_repo->{user}{email} = 'yakex@cpan.org';
+	} else {
+		$conf_repo->{user}{name}  = 'Yak!';
+		$conf_repo->{user}{email} = 'yak_ex@mx.scn.tv';
+	}
+	if($conf_repo->{user}{name} ne $conf_user->{user}{name} ||
+	   $conf_repo->{user}{email} ne $conf_user->{user}{email}) {
+		$conf_repo->write("${repo}/.git/config");
+	}
+}
+
+__END__
+
+=head1 NAME
+
+ghclone.pl - Clone with user configuration
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
