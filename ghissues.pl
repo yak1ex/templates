706b2f664f8a3e7da1a3b5ff660f79488f19398d
Yak! <yak_ex@mx.scn.tv>
2014-02-24 19:10:47 +0900

    Fix for .git suffix in URL.

--- ghissues.pl
+++ ghissues.pl
@@ -68,7 +68,7 @@ sub repo
 		if(-f $conf) {
 			open my $fh, '<', $conf;
 			while(<$fh>) {
-				return $1 if m,\s*url\s*=\s*https://github.com/$user/(.*)(?:\.git)?\s*$,;
+				return $1 if m,\s*url\s*=\s*https://github.com/$user/(.*?)(?:\.git)?\s*$,;
 			}
 			close $fh;
 		}
