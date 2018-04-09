f5978ed48eb8391ef49b51a8994167eaeb93c187
Yak! <yak_ex@mx.scn.tv>
2018-04-09 09:37:00 +0900

    [competitive] Add GCJ2018 source to reflect.

---
+++ competitive/gcj/a.cpp
@@ -0,0 +1,55 @@
+// Cygwin g++ 6.4.0 with -std=c++14
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include <iterator>
+
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <limits>
+
+#include <string>
+
+#include <vector>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+
+#include <tuple>
+#include <initializer_list>
+
+#include <cmath>
+
+typedef unsigned long long ULL;
+typedef long long LL;
+typedef unsigned long UL;
+typedef unsigned int UI;
+typedef unsigned short US;
+typedef unsigned char UC;
+
+#define RNG(v) (v).begin(), (v).end()
+#define REP(v, e) for(UI v = 0U; v < e; ++v)
+#define REP_(v, s, e) for(UI v = s; v < e; ++v)
+#define REPV(v, e) for(v = 0; v < e; ++v)
+#define REPV_(v, s, e) for(v = s; v < e; ++v)
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+
+	UI cases; cin >> cases;
+	REP(casenum, cases) {
+		cout << "Case #" << casenum+1 << ": " << result << endl;
+	}
+
+	return 0;
+}
